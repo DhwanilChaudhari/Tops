27ea763fae4167f643b831789b85c2201888dc6b
DhwanilChaudhari <dhwanil899@gmail.com>
2024-06-22 08:57:33 +0530

    add

--- practice/src/8.Redux2/ReduxHome2/user.js
+++ practice/src/8.Redux2/ReduxHome2/user.js
@@ -1,17 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const userSlice = createSlice({
+export const userSlice = createSlice({
   name: "user",
-  initialState: { user: [] },
+  initialState: {
+    user: [],
+  },
   reducers: {
     addUser: (state, action) => {
       state.user.push(action.payload);
     },
     deleteUser: (state, action) => {
-      state.user = state.user.filter((_, index) => index !== action.payload);
+      state.user.splice(action.payload, 1);
+    },
+    updateUser: (state, action) => {
+      const { index, name } = action.payload;
+      state.user[index] = name;
     },
   },
 });
 
+export const { addUser, deleteUser, updateUser } = userSlice.actions;
+
 export default userSlice.reducer;
-export const { addUser, deleteUser } = userSlice.actions;
--- practice/src/8.Redux2/Ui/User2.jsx
+++ practice/src/8.Redux2/Ui/User2.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addUser, deleteUser } from "../ReduxHome2/user";
+import { addUser, deleteUser, updateUser } from "../ReduxHome2/user";
 
 export default function User2() {
-  let [name, setName] = useState();
-  let [i, setI] = useState();
+  const [name, setName] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+
   const dispatch = useDispatch();
 
-  const data = useSelector((store) => {
-    return store.userSlice;
-  });
+  const data = useSelector((store) => store.userSlice);
 
   const addUserHandler = () => {
-    dispatch(addUser(name));
+    if (editIndex === null) {
+      dispatch(addUser(name));
+    } else {
+      dispatch(updateUser({ index: editIndex, name }));
+      setEditIndex(null);
+    }
     setName("");
   };
-  const deleteHandler = (i) => {
-    dispatch(deleteUser(i));
-    setI("");
+
+  const deleteHandler = (index) => {
+    dispatch(deleteUser(index));
   };
+
+  const editHandler = (name, index) => {
+    setName(name);
+    setEditIndex(index);
+  };
+
   return (
     <div>
       <input
@@ -27,22 +37,21 @@ export default function User2() {
         className="border px-2 py-1"
         onChange={(e) => setName(e.target.value)}
       />
-      <button className="border px-2 py-1" onClick={() => addUserHandler()}>
-        Add
+      <button className="border px-2 py-1" onClick={addUserHandler}>
+        {editIndex === null ? "Add" : "Update"}
       </button>
 
       <hr />
 
       <ul>
-        {data?.user.map?.((e, i) => {
-          return (
-            <div key={i}>
-              <li>
-                {e} <button onClick={() => deleteHandler(i)}>Delete</button>
-              </li>
-            </div>
-          );
-        })}
+        {data?.user.map?.((e, i) => (
+          <div key={i}>
+            <li>
+              {e} <button onClick={() => editHandler(e, i)}>Edit</button>{" "}
+              <button onClick={() => deleteHandler(i)}>Delete</button>
+            </li>
+          </div>
+        ))}
       </ul>
     </div>
   );
